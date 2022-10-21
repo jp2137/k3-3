7811cd464f0583d5aa8cf8811b45654a9cc30402
jp2137 <sbaiyassine2004@gmail.com>
2022-10-21 15:03:16 +0200

    przyciski,etykiety

--- app/src/main/java/com/example/k3_3/MainActivity.kt
+++ app/src/main/java/com/example/k3_3/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.k3_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    val text1 =  findViewById<EditText>(R.id.edittext1).text.toString()
+    val but1 = findViewById<Button>(R.id.button1)
+    val but2 = findViewById<Button>(R.id.button2)
+    val etykieta1 = findViewById<TextView>(R.id.textView1)
+    val etykieta2 = findViewById<TextView>(R.id.textView2)
+    val textEtykiety1 = etykieta1.text.toString()
+    val textEtykiety2 = etykieta1.text.toString()
+
 }
